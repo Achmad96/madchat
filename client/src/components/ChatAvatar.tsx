123a995b3a1d0e2563bbfc8ea50e05b51e9be699
Achmad Raihan <totsukamail3278@gmail.com>
2025-05-29 20:44:47 +0700

    fix<avatar>: change avatar's style

--- client/src/components/ChatAvatar.tsx
+++ client/src/components/ChatAvatar.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 export default function ChatAvatar({ avatar, name }: { avatar?: string; name: string }) {
   return (
     <Avatar className="h-10 w-10">
-      <AvatarImage src={avatar} alt={name} />
+      <AvatarImage src={avatar} alt={name} className="object-cover" />
       <AvatarFallback>{name[0]?.toUpperCase()}</AvatarFallback>
     </Avatar>
   );
