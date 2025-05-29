9addd109e08af8158dbaf920b705643712106ffd
Achmad Raihan <totsukamail3278@gmail.com>
2025-05-30 06:11:20 +0700

    fix<auth>: change auth provider's location

--- client/src/PrimaryLayout.tsx
+++ client/src/PrimaryLayout.tsx
@@ -1,24 +1,21 @@
-import { Outlet } from 'react-router';
-import { AuthProvider } from '@/contexts/AuthContext';
-import { NotificationProvider } from '@/contexts/NotificationContext';
-import { NotificationBadge } from '@/components/NotificationBadge';
-import MenuHamburger from '@/components/MenuHamburger';
+import { Outlet } from "react-router";
+import { NotificationProvider } from "@/contexts/NotificationContext";
+import { NotificationBadge } from "@/components/NotificationBadge";
+import MenuHamburger from "@/components/MenuHamburger";
 
 export default function PrimaryLayout() {
   return (
-    <AuthProvider>
-      <NotificationProvider>
-        <nav className="w-full max-w-full h-[10dvh] px-5 flex justify-center items-center">
-          <div className="flex justify-between items-center w-[50%] max-md:w-full">
-            <h2 className="text-2xl font-bold">MadChat</h2>
-            <div className="flex items-center gap-10">
-              <NotificationBadge />
-              <MenuHamburger />
-            </div>
+    <NotificationProvider>
+      <nav className="w-full max-w-full h-[10dvh] px-5 flex justify-center items-center">
+        <div className="flex justify-between items-center w-[50%] max-md:w-full">
+          <h2 className="text-2xl font-bold">MadChat</h2>
+          <div className="flex items-center gap-10">
+            <NotificationBadge />
+            <MenuHamburger />
           </div>
-        </nav>
-        <Outlet />
-      </NotificationProvider>
-    </AuthProvider>
+        </div>
+      </nav>
+      <Outlet />
+    </NotificationProvider>
   );
 }
--- client/src/components/Providers.tsx
+++ client/src/components/Providers.tsx
@@ -1,9 +1,10 @@
 import { ThemeProvider } from '@/components/theme-provider';
+import { AuthProvider } from '@/contexts/AuthContext';
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      {children}
+      <AuthProvider>{children}</AuthProvider>
     </ThemeProvider>
   );
 }
